598838c251be836865f9e5df5dffd399875d7a38
Daniya <daniya.kurmakaeva@yandex.ru>
2023-06-13 15:26:43 +0300

    Initial commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+
+    public int calculate(int price, int rublesPerMile) {
+        int result;
+       if (price == 10_000) {
+        result = 500;
+       }
+        return result;
+    }
 }
